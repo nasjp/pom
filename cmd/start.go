376a043855b46618ffef85965900d09e902b75e9
y.taniguchi <y.softvalley@gmail.com>
2019-05-08 00:36:31 +0900

    add progress bar

--- cmd/start.go
+++ cmd/start.go
@@ -2,26 +2,47 @@ package cmd
 
 import (
   "fmt"
+  "time"
 
   "github.com/spf13/cobra"
+  pb "gopkg.in/cheggaaa/pb.v2"
+)
+
+// Options represents startCmd's options
+type Options struct {
+  mins int
+}
+
+var (
+  o = &Options{}
 )
 
 // startCmd represents the start command
 var startCmd = &cobra.Command{
   Use:   "start",
   Short: "start pomodoro timer",
-  Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-  Args: cobra.RangeArgs(0, 1),
+  Args:  cobra.NoArgs,
   Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("start called")
+
+    fmt.Printf("start pomodoro in %d minutes!!\n", o.mins)
+
+    // Start parallel processing
+    outputBar(`🍅  : {{etime .}} / {{string . "minutes"}}m ( {{percent . }} ) {{bar . "|" ">" ">" "-" "|" | green}} `)
   },
+}
+
+func outputBar(tmpl string) {
+  secs := 60 * o.mins
+  bar := pb.ProgressBarTemplate(tmpl).Start(secs)
+  bar.Set("minutes", o.mins)
+  defer bar.Finish()
+  for i := 0; i < secs; i++ {
+    bar.Add(1)
+    time.Sleep(time.Second)
+  }
 }
 
 func init() {
   RootCmd.AddCommand(startCmd)
+  startCmd.Flags().IntVarP(&o.mins, "set", "s", 25, "set the timer")
 }
