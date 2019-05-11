d8e2550f1b5aaf8a11f51787907f378d2423f105
y.taniguchi <y.softvalley@gmail.com>
2019-05-11 15:48:35 +0900

    import file in main.go

--- cmd/pom/main.go
+++ cmd/pom/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/YukihiroTaniguchi/pom/domain/service/command"
+	"github.com/YukihiroTaniguchi/pom/interface/file"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +16,10 @@ func init() {
 
 func main() {
 	if err := command.RootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(-1)
+	}
+	if err := file.CreateOrNot("hoge/huga"); err != nil {
 		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 		os.Exit(-1)
 	}
