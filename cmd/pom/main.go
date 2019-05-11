fa42cbd8a261163f6ffd911ff7da441b26a238cf
y.taniguchi <y.softvalley@gmail.com>
2019-05-12 00:59:13 +0900

    rename .pomrc to pom.json

--- cmd/pom/main.go
+++ cmd/pom/main.go
@@ -15,18 +15,13 @@ const (
 	GOPATH = "GOPATH"
 	// APPDIR ...
 	APPDIR = "/src/github.com/YukihiroTaniguchi/pom"
+	// FILE ...
+	FILE = "/config/pom.json"
 )
 
 func init() {
 	cobra.OnInitialize()
 	command.RootCmd.AddCommand(command.StartCmd)
-}
-
-func main() {
-	if err := command.RootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
-		os.Exit(-1)
-	}
 
 	ev, err := environment.GetEnvVar(GOPATH)
 	if err != nil {
@@ -34,7 +29,14 @@ func main() {
 		os.Exit(-1)
 	}
 
-	if err := file.CreateOrNot(ev.Dir + APPDIR + "/config/.pomrc"); err != nil {
+	if err := file.CreateOrNot(ev.Dir + APPDIR + FILE); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(-1)
+	}
+}
+
+func main() {
+	if err := command.RootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 		os.Exit(-1)
 	}
