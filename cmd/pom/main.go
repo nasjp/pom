509eabff526a8e7dadfdacbb38c39c070d72e775
y.taniguchi <y.softvalley@gmail.com>
2019-05-12 13:12:07 +0900

    refix config file variable name in main.go

--- cmd/pom/main.go
+++ cmd/pom/main.go
@@ -15,8 +15,8 @@ const (
 	GOPATH = "GOPATH"
 	// APPDIR ...
 	APPDIR = "/src/github.com/YukihiroTaniguchi/pom"
-	// CONFIG_FILE ...
-	CONFIG_FILE = "/config/pom.json"
+	// CONFIGFILE ...
+	CONFIGFILE = "/config/pom.json"
 )
 
 func init() {
@@ -29,7 +29,7 @@ func init() {
 		os.Exit(-1)
 	}
 
-	if err := file.CreateOrNot(ev.Dir + APPDIR + CONFIG_FILE); err != nil {
+	if err := file.CreateOrNot(ev.Dir + APPDIR + CONFIGFILE); err != nil {
 		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 		os.Exit(-1)
 	}
